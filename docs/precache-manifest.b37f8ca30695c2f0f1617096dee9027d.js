1213f821e22df38e51518a27d5c7e3e709264821
AndresC <andres_fel94@hotmail.com>
2020-11-09 11:17:37 -0500

    docs folder added

---
+++ docs/precache-manifest.b37f8ca30695c2f0f1617096dee9027d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd6e85cef4b4f7257464f2c93c04aba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d864d600c10500d3a79",
+    "url": "/static/css/main.9d34c824.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d864d600c10500d3a79",
+    "url": "/static/js/main.2ec51923.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
